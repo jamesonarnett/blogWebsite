00cafd286cd645387527d8d7c681e6ddc8cb67a0
Jameson <90803725+jamesonarnett@users.noreply.github.com>
2021-11-08 19:22:02 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const homeStartingContent = "Lets see if this baby really even works celerisque eleifend donec pretium vulputate sapien. Rhonc celerisque eleifend donec pretium vulputate sapien. Rhonc celerisque eleifend donec pretium vulputate sapien. Rhonc celerisque eleifend donec pretium vulputate sapien. Rhonc celerisque eleifend donec pretium vulputate sapien. Rhonc celerisque eleifend donec pretium vulputate sapien. Rhonc celerisque eleifend donec pretium vulputate sapien. Rhonc celerisque eleifend donec pretium vulputate sapien. Rhonc celerisque eleifend donec pretium vulputate sapien. Rhonc ";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+let blogPosts = [];
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+   res.render('home', {homeStartingContent: homeStartingContent,
+    blogPosts: blogPosts,})
+})
+
+app.get("/about", function(req, res) {
+  res.render('about', {aboutContent: aboutContent})
+})
+
+app.get("/contact", function(req, res) {
+  res.render('contact', {contactContent: contactContent})
+})
+
+app.get("/compose", function(req, res) {
+  res.render('compose')
+})
+
+app.post("/compose", function(req, res) {
+  const post = {
+    title: req.body.postTitle,
+    body: req.body.textArea
+  }
+  blogPosts.push(post);
+  res.redirect("/");
+})
+
+
+app.get("/blogPosts/:postName", function(req, res) {
+  let requestedTitle = _.lowerCase(req.params.postName)
+
+    blogPosts.forEach(function(post){
+      const storedTitle = _.lowerCase(post.title)
+
+      if (storedTitle === requestedTitle) {
+        res.render('post', {
+          title: post.title,
+          content: post.body
+        })
+      }
+    })
+})
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
+app.listen(process.env.PORT || 3000);
